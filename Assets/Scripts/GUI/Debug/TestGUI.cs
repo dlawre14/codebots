aa9bb80ebf5d460dec8ecea066224fe8dba47865
Dylan Lawrence <dlawre14@slu.edu>
2014-09-17 15:07:30 -0500

    gui is not working, porting to beta unity

--- Assets/Scripts/GUI/Debug/TestGUI.cs
+++ Assets/Scripts/GUI/Debug/TestGUI.cs
@@ -19,15 +19,13 @@ public class TestGUI : MonoBehaviour {
 		GUI.Label (position, "<color=" + textColor + ">" + text + "</color>", style);
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	private float xpos = -100;
+	private bool popup = false;
+	private bool rendered = true;
+
+	private string angle;
+
+	private Rect windowrect = new Rect(Screen.width/2.0f - 125, Screen.height/2.0f - 50,250,100);
 
 	void OnGUI() {
 		GUIStyle style = new GUIStyle();
@@ -36,5 +34,60 @@ public class TestGUI : MonoBehaviour {
 		style.alignment = TextAnchor.MiddleCenter;
 
 		OutlineLabel (new Rect(Screen.width/2.0f - 100/2.0f,0,100,20), "Codebots -- Debug Client", style);
+
+		/** Left Side Panel */
+		GUILayout.BeginArea(new Rect(xpos, Screen.height/2.0f - 150, 100, 300));
+
+		if (GUILayout.Button ("Forward")) {
+			//Do Stuff
+		}
+		if (GUILayout.Button ("Left Turn")) {
+			popup = true;
+		}
+		if (GUILayout.Button ("Right Turn")) {
+			//Do Stuff
+		}
+		if (GUILayout.Button ("Reverse")) {
+			//Do Stuff
+		}
+
+		GUILayout.EndArea();
+
+		/** Left Side Panel Toggl */
+		GUILayout.BeginArea (new Rect(xpos + 100, Screen.height/2.0f - 25, 30, 50));
+
+		if (xpos < 0) {
+			if (GUILayout.Button (">>"))
+				xpos += 100;
+		}
+		else {
+			if (GUILayout.Button ("<<"))
+				xpos -= 100;
+		}
+
+		GUILayout.EndArea();
+
+		//popup window
+		if (popup) {
+			windowrect = GUI.ModalWindow (0, windowrect, PromptWindow, "Customize Maneuver");
+		}
+	}
+
+
+
+	//Pop-up Window Fumctions
+	void PromptWindow(int windowid) {
+		GUILayout.BeginArea(new Rect(10,20,200,200));
+
+		GUILayout.Label("Enter degrees to move...");
+		angle = GUILayout.TextField(angle);
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Button ("Confirm");
+			GUILayout.Button ("Cancel");
+
+			GUILayout.EndHorizontal();
+
+		GUILayout.EndArea();
 	}
 }
