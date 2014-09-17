12b2e205f8f7adc683abbf256f82395ed03ab625
Dylan Lawrence <dlawre14@slu.edu>
2014-09-17 18:24:28 -0500

    refined methodology for handling gui events

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -4,17 +4,35 @@ using UnityEngine.UI;
 
 public class GUIManager : MonoBehaviour {
 
-	//Because the UI Layouts Exist We Can Simply Collect Them Through the Canvas
+	//Because the UI Layouts Exist We Will Simply Define Functions Here For Use In the Code
 	
-	void Start() {
-
-	}
+	private bool docked = true;
 	
-	void OnGUI() {
-		
-	}
-	
+	//Functions for buttons
 	public void SlideClicked() {
-		Debug.Log ("stuff");
+		if (docked)
+			StartCoroutine("SlideOut");
+		else
+			StartCoroutine("SlideIn");
+	}
+	
+	//Internal Functions
+	IEnumerator SlideOut() {
+		docked = false;
+		transform.Find ("Side Panel/Slide Button/Slide Text").GetComponent<Text>().text = "<<";
+
+		for (int i = 0; i<128; i+=3) {
+			transform.Find("Side Panel").GetComponent<RectTransform>().anchoredPosition = transform.Find("Side Panel").GetComponent<RectTransform>().anchoredPosition + new Vector2(3,0);
+			yield return new WaitForSeconds(Time.deltaTime);
+		}
+	}
+	
+	IEnumerator SlideIn(){
+		docked = true;
+		transform.Find ("Side Panel/Slide Button/Slide Text").GetComponent<Text>().text = ">>";
+		for (int i = 0; i<128; i+=3) {
+			transform.Find("Side Panel").GetComponent<RectTransform>().anchoredPosition = transform.Find("Side Panel").GetComponent<RectTransform>().anchoredPosition - new Vector2(3,0);
+			yield return new WaitForSeconds(Time.deltaTime);
+		}
 	}
 }
