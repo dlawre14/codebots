cc2922e1dc5acd83d12174dc20f9fb253a231e78
Dylan Lawrence <dlawre14@slu.edu>
2014-09-17 16:52:13 -0500

    using beta unity with new UI features, seems promising, but time consuming

---
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+	//Because the UI Layouts Exist We Can Simply Collect Them Through the Canvas
+	
+	void Start() {
+
+	}
+	
+	void OnGUI() {
+		
+	}
+	
+	public void SlideClicked() {
+		Debug.Log ("stuff");
+	}
+}
