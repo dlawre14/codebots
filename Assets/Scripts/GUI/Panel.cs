758a2e3aaf94ac72e8913e2de80f9d55a202c6d5
Dylan Lawrence <dlawre14@slu.edu>
2014-09-18 18:50:50 -0500

    finally have a good gui prototype in the works

---
+++ Assets/Scripts/GUI/Panel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Panel : MonoBehaviour {
+	
+	[Tooltip ("This contains all objects in the panel")]
+	public GameObject[] contains;
+	
+	private bool working;
+	private bool open;
+	
+	void Start() {
+		working = false;
+		open = false;
+	}
+	
+	public void CallSlideHorizontal(int amount) {
+		if (!working) {
+			StartCoroutine(SlideHorizontal(amount));
+			working = true;
+			open = !open;
+		}
+	}
+	
+	public IEnumerator SlideHorizontal(int amount) {
+		if (!open) {
+			for (int i = 0; i < amount; i++) {
+				foreach (GameObject g in contains)
+					g.GetComponent<RectTransform>().anchoredPosition = g.GetComponent<RectTransform>().anchoredPosition + new Vector2 (1,0);
+				yield return new WaitForSeconds(Time.deltaTime);
+			}
+		}
+		else {
+			for (int i = 0; i < amount; i++) {
+				foreach (GameObject g in contains)
+					g.GetComponent<RectTransform>().anchoredPosition = g.GetComponent<RectTransform>().anchoredPosition - new Vector2 (1,0);
+				yield return new WaitForSeconds(Time.deltaTime);
+			}
+		}
+		working = false;
+	}
+	
+	
+	
+}
---
+++ Assets/Scripts/GUI/TextChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextChange : MonoBehaviour {
+	
+	[Tooltip ("Object containing the UI Text Component")]
+	public GameObject obj;
+	
+	private bool open = false;
+	
+	public void ChangeText() {
+		if (!open) {
+			obj.GetComponent<Text>().text = "<<";
+			open = true;
+		}
+		else {
+			obj.GetComponent<Text>().text = ">>";
+			open = true;
+		}
+	}	
+}
