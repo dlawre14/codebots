b3a5e3401c860bd68902efe117ca90c54f94ad7b
Dylan Lawrence <dlawre14@slu.edu>
2014-09-21 16:05:53 -0500

    working on some basic map stuff, camera controls (simple) done

---
+++ Assets/Scripts/CameraControls/GameCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+	
+	public GameObject map;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.mousePosition.x < 15)
+			transform.RotateAround(map.transform.position, Vector3.up, 20 * Time.deltaTime);
+		else if (Input.mousePosition.x > Screen.width - 15)
+			transform.RotateAround(map.transform.position, Vector3.up, -20 * Time.deltaTime);
+			
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			transform.Translate(Vector3.forward * Time.deltaTime * 10);
+		}
+		else if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			transform.Translate(-Vector3.forward * Time.deltaTime * 10);
+		}
+	}
+}
