d3e78b5c9d145c28853f0e33b730d17d6af3dff6
Dylan Lawrence <dlawre14@slu.edu>
2014-09-22 17:00:01 -0500

    editor is likely a late stage goal now

---
+++ Assets/Scripts/Text Editor/TextEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextEditor : MonoBehaviour {
+	
+	public Text main;
+	public Text linecount;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
