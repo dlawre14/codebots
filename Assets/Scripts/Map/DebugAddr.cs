33ec683535228804f59f3369fed1f355566f97ef
Dylan Lawrence <dlawre14@slu.edu>
2014-09-23 22:28:55 -0500

    map editor is partially done

---
+++ Assets/Scripts/Map/DebugAddr.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugAddr : MonoBehaviour {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddPanel() {
+		GameObject g = (GameObject) Instantiate (panel);
+		g.transform.parent = transform;
+	}
+}
