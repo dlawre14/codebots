64bb6d92f3b224069dcb0ebcb5e3b2ad88802fa7
Dylan Lawrence <dlawre14@slu.edu>
2014-09-24 13:11:52 -0500

    still working on the editor

---
+++ Assets/Scripts/Map/InputBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InputBox : MonoBehaviour {
+
+	private int x = 0;
+	private int y = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetPosition(int sx, int sy) {
+		x = sx;
+		y = sy;
+
+		GetComponentInChildren<Text> ().text = "(" + x + "," + y + ")";
+	}
+}
