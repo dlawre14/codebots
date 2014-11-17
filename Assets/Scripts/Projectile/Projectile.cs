48fd47cbaa8eb524e9bf9c06b01dbb63459443d3
Dylan Lawrence <dlawre14@slu.edu>
2014-11-17 11:31:09 -0600

    cleaned up old scenes, transitioning to in-engine map maker work

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce (new Vector3 (0, 0, force));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter() {
+		rigidbody.isKinematic = true;
+		GetComponent<MeshRenderer> ().enabled = false;
+
+		ParticleSystem s = GetComponent<ParticleSystem> ();
+		s.Play ();
+
+		StartCoroutine(StartDestroy ());
+	}
+
+	IEnumerator StartDestroy() {
+		yield return new WaitForSeconds(1.5f);
+		Destroy (gameObject);
+	}
+}
